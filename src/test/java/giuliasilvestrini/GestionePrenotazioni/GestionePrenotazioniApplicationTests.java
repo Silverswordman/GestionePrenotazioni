6146de1c083ad00f155619186bf39afdef690731
Giulia Silvestrini <silvergiuly@hotmail.it>
2024-01-12 10:48:33 +0100

    Initial commit

---
+++ src/test/java/giuliasilvestrini/GestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
